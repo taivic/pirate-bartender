684014f3a81730bfa1c5bfbd463c2d4c8d981ab3
taivic <vicky.yen.tai@gmail.com>
2016-11-14 13:20:04 -0600

    updated UI

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
 	
-	var Ingredient = function(name, type) {
+	//object ingredient
+	var Ingredient = function(name, type, drinkName) {
 		this.name = name;
 		this.type = type;
+		this.drinkName = drinkName;
 	}
 
+	//object pantry
 	var Pantry = function(name) {
 		this.name = name;
 		this.ingredients = {};
 	}
 
+	//push ingredient name and type to pantry
 	Pantry.prototype.addIngredient = function(ingredient) {
 		if (this.ingredients[ingredient.type]) {
 			this.ingredients[ingredient.type].push(ingredient.name);
@@ -18,32 +22,53 @@ $(document).ready(function(){
 		}
 	}
 
+	//object customer
 	var Customer = function(name) {
 		this.name = name;
 		this.preferences = [];
 		this.favorite = {};
 	}
 
+	//object drink the bartender makes
 	var Drink = function(name, ingredients) {
 		this.name = name;
 		this.ingredients = ingredients;
 	}
 
+	var myDrink = new Drink("myDrink");
+	var guest = new Customer(name)
+	//create drink from preferences
+	Drink.prototype.makeDrink = function(Customer, Ingredient) {
+		if (guest.preferences.length > 0) {
+			//find random ingredients that match preferences
+			for (var i = 0; i < guest.preferences.length; i++) {			
+				console.log(guest.preferences[i]);	
+			//insert adjectives
+			}
+		} else {
+			var html = "<p>" + "A glass of distilled sea water." + "</p>"
+			$("#drinks").html(html);    
+		}
+	}
+
+	//object worker which includes bartender
 	var Worker = function(name) {
 		this.name = name;
 		this.customers = {};
 	}
 
+	//prompt by the bartender, customer name gets pushed to customer object
 	Worker.prototype.greet = function() {
 		var name = prompt("Ahoy matey! What say your name?");
 		if (this.customers[name]) {
-			alert("Welcome back, here is your favorite drink " + this.customers[name].favorite.name);
+			alert("Welcome back, here is your favorite drink: " + this.customers[name].favorite.name);
 		} else {
 			guest = new Customer(name);
 			this.customers[name] = guest;
 		}
 	}
 
+	//bartender object is a worker, bartender asks an array of questions
 	var Bartender = function(name) {
 		Worker.call(this, name);
 		this.questions = [];
@@ -51,20 +76,30 @@ $(document).ready(function(){
 
 	Bartender.prototype = Object.create(Worker.prototype);
 	Bartender.prototype.constructor = Bartender;
+	
+	//bartender's questions are asked, push question to questions
 	Bartender.prototype.addQuestion = function(question) {
 		this.questions.push(question);
 	}
-	Bartender.prototype.askQuestion = function(num) {
-		if (num < this.questions.length) {		
-			var html = '<h2>' + this.questions[num].text + '</h2>' +
-				'<select id="answer"><option value="yea">Yea</option><option value="nay">Nay</option></select>' +
-				'<button id="questionSubmit">Submit</button>'
+
+	Bartender.prototype.askQuestion = function(questionCount) {
+		if (questionCount < this.questions.length) {		
+			var html = '<h2>' + this.questions[questionCount].text + '</h2>' +
+				'<br>' +
+				'<select class="form-control input-lg" id="answer"><option value="" disabled selected>Select...or else...!</option><option value="yea">Yea</option><option value="nay">Nay</option></select>' +
+				'<br>' +
+				'<button id="questionSubmit" class="btn btn-danger btn-lg btn-block">Submit</button>'
 			$('#question').html(html);
 		} else {
+			$('#question').hide();
+			var html = '<h2>' + "This is what you asked for..." + '</h2>' //+DRINK NAME
+			$('#results').html(html);
 			console.log('making drink');
+			myDrink.makeDrink(Customer, Ingredient);
 		}
 	}
 
+	//question is an object
 	var Question = function(text, type) {
 		this.text = text;
 		this.type = type;
@@ -72,22 +107,48 @@ $(document).ready(function(){
 
 	var myPantry = new Pantry("myPantry");
 
-	var newIngredient = new Ingredient("Slug of Rum", "strong");
+	var newIngredient = new Ingredient("Slug of Rum", "strong", "Slug");
 	myPantry.addIngredient(newIngredient);
-	var newIngredient = new Ingredient("Shot of Whisky", "strong");
+	var newIngredient = new Ingredient("Shot of Whisky", "strong", "Shot");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Splash of Gin", "strong", "Splash");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Olive on a stick", "salty", "Olive");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Salt-dusted rim", "salty", "Dusted");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Rasher of bacon", "salty", "Bacon");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Shake of bitters", "bitter", "Bitters");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("splash of tonic", "bitter", "Tonic");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("twist of lemon peel", "bitter", "Twist");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Sugar cube", "sweet", "Cube");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("spoonful of honey", "sweet", "Honey");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("splash of cola", "sweet", "Cola");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("Slice of orange", "fruity", "Orange");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("dash of cassis", "fruity", "Cassis");
+	myPantry.addIngredient(newIngredient);
+	var newIngredient = new Ingredient("cherry on top", "fruity", "Cherry");
 	myPantry.addIngredient(newIngredient);
 
 	var bartender = new Bartender("Vic");
 
-	var question = new Question("Do you like your drink strong?", "strong");	
+	var question = new Question("Do ye like yer drinks strong?", "strong");	
 	bartender.addQuestion(question);
-	var question = new Question("Do you like your drink salty?", "salty");	
+	var question = new Question("Do ye like it with a salty tang?", "salty");	
 	bartender.addQuestion(question);
-	var question = new Question("Do you like your drink sweet?", "sweet");	
+	var question = new Question("Are ye a lubber who likes it bitter?", "sweet");	
 	bartender.addQuestion(question);
-	var question = new Question("Do you like your drink bitter?", "bitter");	
+	var question = new Question("Would ye like a bit of sweetness with yer poison?", "bitter");	
 	bartender.addQuestion(question);
-	var question = new Question("Do you like your drink fruity?", "fruity");	
+	var question = new Question("Are ye one for a fruity finish?", "fruity");	
 	bartender.addQuestion(question);
 
 	var guest;
